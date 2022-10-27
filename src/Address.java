ca02d142df5e843f48c31d29ae7750880f41ddfb
Oleksandr Berezhnyi <oberezhnyi@gmail.com>
2022-10-27 19:13:09 +0300

    Created class Address

---
+++ src/Address.java
@@ -0,0 +1,74 @@
+public class Address {
+    private Integer index;
+    private String country;
+    private String city;
+    private String street;
+    private String house;
+    private String apartment;
+
+    public Address (Integer index,
+                    String country,
+                    String city,
+                    String street,
+                    String house,
+                    String apartment)
+    {
+        this.index = index;
+        this.country = country;
+        this.city = city;
+        this.street = street;
+        this.house = house;
+        this.apartment = apartment;
+    }
+
+    public Address() {
+    }
+
+    public Integer getIndex() {
+        return index;
+    }
+
+    public void setIndex (Integer index) {
+        this.index = index;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
+
+    public String getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(String apartment) {
+        this.apartment = apartment;
+    }
+}
