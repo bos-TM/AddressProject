74c28f1ede4a7293c47fbf5aa518d85eb1042623
Oleksandr Berezhnyi <oberezhnyi@gmail.com>
2022-10-27 19:32:24 +0300

    Made class Address and made out data of address

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Address personAddress = new Address(
+                1,
+                "Україна",
+                "Івано-Франківськ",
+                "Шевченка",
+                "23Б",
+                "1-А"
+        );
+
+    System.out.println("Поштова адреса " + personAddress.getIndex() + ":");
+    System.out.println("країна: " + personAddress.getCountry());
+    System.out.println("місто: " + personAddress.getCity());
+    System.out.println("вулиця: " + personAddress.getStreet());
+    System.out.println("будинок: " + personAddress.getHouse());
+    System.out.println("квартира: " + personAddress.getApartment());
     }
 }
